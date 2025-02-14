8d7482abf3914daf279718fe330474d8fa238e50
Kevin Sheehan <kshhn12@gmail.com>
2025-02-13 21:40:00 -0500

    updated

--- Whiteboard practice app/obj/Debug/net8.0/Whiteboard practice app.AssemblyInfo.cs
+++ Whiteboard practice app/obj/Debug/net8.0/Whiteboard practice app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Whiteboard practice app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53f4a68fa3e3f2e4c856dc37b1a14d25bf803e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce88f6045ffcaf571705e15220a5a29a8233d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Whiteboard practice app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Whiteboard practice app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
